8b042fd9c6c5fcc85b3c41f8b7c6406a00f4f178
Aditya-011 <adihgr18@gmail.com>
2021-02-24 13:51:38 +0530

    api tweaks done

--- config/db.js
+++ config/db.js
@@ -1,16 +1,21 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
+// function to connect cloud storage "connectDB()"
 function connectDB() {
-    // Database connection 🥳
-    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
-    const connection = mongoose.connection;
-    connection.once('open', () => {
-        console.log('Database connected 🥳🥳🥳🥳');
-    }).catch(err => {
-        console.log('Connection failed ☹️☹️☹️☹️');
+  mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  });
+  const connection = mongoose.connection;
+  connection
+    .once("open", () => {
+      console.log("DB Connected Successfully");
+    })
+    .catch((err) => {
+      console.log(err);
     });
 }
-
-// mIAY0a6u1ByJsWWZ
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-require('dotenv').config();
-const express = require('express');
+//imports
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const path = require('path');
-const cors = require('cors');
-// Cors 
+const path = require("path");
+const cors = require("cors");
+
+// adding allowed clients api
 const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
+  origin: process.env.ALLOWED_CLIENTS,
+};
 
 // Default configuration looks like
 // {
@@ -18,21 +19,21 @@ const corsOptions = {
 //     "optionsSuccessStatus": 204
 //   }
 
-app.use(cors(corsOptions))
-app.use(express.static('public'));
+app.use(cors(corsOptions));
+app.use(express.static("public"));
 
-const connectDB = require('./config/db');
+// connection to cloud storage
+const connectDB = require("./config/db");
 connectDB();
 
 app.use(express.json());
+// setting ejs as template engine
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs');
-
-// Routes 
-app.use('/api/files', require('./routes/files'));
-app.use('/files', require('./routes/show'));
-app.use('/files/download', require('./routes/download'));
-
+// Routes
+app.use("/api/files", require("./routes/files"));
+app.use("/files", require("./routes/show"));
+app.use("/files/download", require("./routes/download"));
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
